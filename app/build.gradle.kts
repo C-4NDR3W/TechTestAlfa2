42ac5472f5ef1de41f1fccb702227cc87559703b
Andrew <clarenceandrew9105@gmail.com>
2024-12-25 01:43:19 +0700

    Added splash screen and loading animation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/com/example/techtestalfa2/LoginActivity.kt
+++ app/src/main/java/com/example/techtestalfa2/LoginActivity.kt
@@ -2,21 +2,33 @@ package com.example.techtestalfa2
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //Install pertama tidak menunjukkan splash screen
+        installSplashScreen()
         setContentView(R.layout.activity_login)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
+        val studentHeader = findViewById<TextView>(R.id.appName)
         val usernameInput = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.usernameInput)
         val passwordInput = findViewById<com.google.android.material.textfield.TextInputEditText>(R.id.passwordInput)
         val loginButton = findViewById<Button>(R.id.loginButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        progressBar.visibility = ProgressBar.INVISIBLE
 
         loginButton.setOnClickListener {
             val username = usernameInput.text.toString().trim()
@@ -27,12 +39,22 @@ class LoginActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
+            progressBar.visibility = ProgressBar.VISIBLE
+            studentHeader.visibility = View.GONE
+            usernameInput.visibility = View.GONE
+            passwordInput.visibility = View.GONE
+            loginButton.visibility = View.GONE
+
             if (username == "alfagift-admin" && password == "asdf") {
-                Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                finish()
+                progressBar.postDelayed({
+                    Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show()
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }, 1500)
             } else {
+                progressBar.visibility = ProgressBar.INVISIBLE
                 Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
             }
         }
